eeb715bdc2773ebda295552f637ece767cf885a7
Rogerio Gama <rogerio.gama@deliverit.com.br>
2022-11-14 11:24:49 -0300

    Salvando passos antes de configurar DTO

--- src/main/java/one/digitalinnovation/personapi/dto/request/PersonDTO.java
+++ src/main/java/one/digitalinnovation/personapi/dto/request/PersonDTO.java
@@ -4,7 +4,11 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.br.CPF;
 
+import javax.validation.Valid;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 import java.util.List;
 
 @Data
@@ -12,10 +16,23 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 public class PersonDTO {
+
     private Long id;
+
+    @NotEmpty
+    @Size(min = 2, max = 100)
     private String firstName;
+
+    @NotEmpty
+    @Size(min = 2, max = 100)
     private String lastname;
+
+    @NotEmpty
+    @CPF
     private String cpf;
     private String birthDate;
+
+    @Valid
+    @NotEmpty
     private List<PhoneDTO> phones;
 }
